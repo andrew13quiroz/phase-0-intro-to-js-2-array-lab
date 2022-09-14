7976ad2ef1b24810ac7d95940b2967c8ba3a6a64
Andrew Quiroz <andrew13quiroz13@gmail.com>
2022-09-14 02:00:45 -0500

    commit tree 5ec94ff6f307770498d3e1470c873871149f78dd

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(string) {
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat(string) {
+    cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(string) {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(string){
+    cats.shift();
+};
+
+function appendCat(name) {
+    const appendCat = [...cats, name];
+    return appendCat;
+}
+
+function prependCat(name) {
+    const prependCat = [name, ...cats];
+    return prependCat;
+}
+
+function removeLastCat(name) {
+    const removeLastCat = cats.slice(name, 2);
+    return removeLastCat;
+}
+
+function removeFirstCat(name) {
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
